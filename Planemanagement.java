238a57b4dfa8d0eca28212b503443d698a3ba6fc
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-05-21 09:25:55 +0530

    added buy seat method to the code
    from this method I get seat number and row as inputs and check these inputs are valid and through this get user's name, email, and surname. And also calculate the ticket price.

--- Planemanagement.java
+++ Planemanagement.java
@@ -57,7 +57,8 @@ public class Planemanagement {
             }
         }
     }
-
+    /*this method prints the menu
+     */
     public static void print_menu(){
         System.out.println("***************************************************");
         System.out.println("*                    Menu                         *");
@@ -71,5 +72,83 @@ public class Planemanagement {
                         \t6) Search tickets
                         \t0) Quit""");
         System.out.println("***************************************************");
+    }
+
+    //    buy seat method
+    public static void buy_seat() {
+        System.out.println("Enter seat row and number that you want to book.\n");
+        while (true) {
+            try{
+                System.out.print("Enter seat row letter (A/B/C/D) :"); //get the seat row letter user want to book
+                char seat_row = Character.toUpperCase(input.next().charAt(0));
+
+                //check is it a valid roe letter or not? if it is a valid roe letter it returns a rowindex number
+                int rowIndex = -1;
+                switch (seat_row) {
+                    case 'A':
+                        rowIndex = 0;
+                        break;
+                    case 'B':
+                        rowIndex = 1;
+                        break;
+                    case 'C':
+                        rowIndex = 2;
+                        break;
+                    case 'D':
+                        rowIndex = 3;
+                        break;
+                    default:
+                        System.out.println("Invalid seat row. Enter valid seat row.");
+                        continue; //again asks row?
+                }
+
+                System.out.print("Enter seat number :");//get seat number
+                int seat_column = input.nextInt();
+
+                //check the entered seat number is between the range, if the input is wrong again asks the qn
+                if (seat_column < 1 || seat_column > seat_order[rowIndex].length) {
+                    System.out.println("Invalid seat number. Please enter row and seat number again");
+                    continue; //if the user enter wrong input again asks for row and seat column
+                }
+//                according to the rowindex number and seat number this checks the 2D array and the required index is 0 programs asks
+//                the name,email and surname .from these details person class create an object
+                if (seat_order[rowIndex][seat_column - 1] == 0) {
+                    seat_order[rowIndex][seat_column - 1] = 1; //if seat is not booked it books suite seat using user inputs
+                    System.out.println("\nEnter your pesonal details to book the seat\n");
+                    //collect person data
+                    System.out.print("Enter your name : ");
+                    String name = input.next();
+                    System.out.print("Enter your Surname : ");
+                    String surname = input.next();
+                    System.out.print("Enter a email address : ");
+                    String email = input.next();
+                    //call the person object and create a new person using entered details
+                    Person person = new Person(name, surname, email);
+
+                    //ticket price
+                    int price = calculate_price(seat_column);
+
+//                     creating a new ticket
+                    Ticket ticket = new Ticket(seat_row, seat_column, price, person); //call ticket object
+                    ticket.save(); //save method called to save the text file
+                    System.out.println("\nYour seat successfully booked and ticket downloaded");
+//                  through the collected data ticket class creates ticket and from this for loop those data insert in to list
+                    for (int y = 0; y <= tickets.length; y++) {
+                        if (tickets[y] == null) {
+                            tickets[y] = new Ticket(seat_row, seat_column, price, person); //insert data into the ticket array
+                            break;
+                        }
+                    }
+                    break;
+                } else {
+                    System.out.println("Sorry Seat already booked!");
+                }
+//                when the user enter a letter for the seat_number the program crashes, to prevent it this try catch used
+            }catch (InputMismatchException e){
+                System.out.println("\nEnter a valid input for seat number!\n");
+                input.next();
+            }
+
+        }
     }
 }
